5190c4f5fdf493b1b71050ecfd4df634d767004d
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-07 00:34:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { MessageFormComponent, MessageItemComponent, MessageListComponent } from './chatbot/components';
+import { DialogflowService } from './chatbot/services';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatbotComponent,
+    MessageListComponent,
+    MessageFormComponent,
+    MessageItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DialogflowService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
